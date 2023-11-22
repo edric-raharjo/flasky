f60afce7f9a6c5df713c185be75b4fb2c0dc757a
EDRIC BOBY <boby.triraharjo@gmail.com>
2023-11-22 16:10:25 +0700

    change ip

--- deploy/wsgi.py
+++ deploy/wsgi.py
@@ -5,6 +5,6 @@ from Main import app as application
 
 
 if __name__ == "__main__":
-    host = os.environ.get("FLASK_HOST", "0.0.0.0")
-    port = os.environ.get("FLASK_PORT", "80")
+    host = os.environ.get("FLASK_HOST", "127.0.0.1")
+    port = os.environ.get("FLASK_PORT", "8001")
     application.run(host=host, port=port)
